ae978ebb2791caad7f694c08b1eb3fbbe8e38481
Jinthebandit <jinthebandit@gmail.com>
2019-10-02 08:56:02 +0200

    Project start

---
+++ Multiplikationstabelle-C/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Jinthebandit
+ * dafasdfa
+ *
+ * Created on 2. Oktober 2019, 08:54
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
